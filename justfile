4a58cbfc359b86bc53bfd8c01bc9f24b0ead8ef5
Seun LanLege <seunlanlege@gmail.com>
2018-03-25 18:37:42 +0100

    added reactor core to FakeReactor

--- justfile
+++ justfile
@@ -1,2 +1,2 @@
 test:
-	cargo test -- --test-threads 1 --nocapture
+	cargo test -- --nocapture
--- src/contrib/fakereactor.rs
+++ src/contrib/fakereactor.rs
@@ -1,15 +1,15 @@
 use routing::Router;
-use futures::Future;
-use core::{Response, Request};
-use proto::{ArcService};
-use hyper::{Method, Uri, Headers, HttpVersion, Body};
+use core::{Request, Response};
+use proto::ArcService;
+use hyper::{Body, Headers, HttpVersion, Method, Uri};
 use serde::ser::Serialize;
 use anymap::AnyMap;
 use std::str::FromStr;
-use serde_json::{to_vec};
+use serde_json::to_vec;
+use tokio_core::reactor::Core;
 
 pub struct FakeReactor {
-	pub routes: Router
+	pub routes: Router,
 }
 
 impl FakeReactor {
@@ -17,19 +17,15 @@ impl FakeReactor {
 		&self,
 		route: &str,
 		body: Option<T>,
-		headers: Option<Headers>
-	) -> Box<Future<Item=Response, Error=Response>>
-		where T: Serialize
+		headers: Option<Headers>,
+	) -> Result<Response, Response>
+	where
+		T: Serialize,
 	{
 		self.build(Method::Post, route, body, headers)
 	}
 
-	pub fn get(
-		&self,
-		route: &str,
-		headers: Option<Headers>
-	) -> Box<Future<Item=Response, Error=Response>>
-	{
+	pub fn get(&self, route: &str, headers: Option<Headers>) -> Result<Response, Response> {
 		self.build(Method::Get, route, None::<u8>, headers)
 	}
 
@@ -37,9 +33,10 @@ impl FakeReactor {
 		&self,
 		route: &str,
 		body: Option<T>,
-		headers: Option<Headers>
-	) -> Box<Future<Item=Response, Error=Response>>
-		where T: Serialize
+		headers: Option<Headers>,
+	) -> Result<Response, Response>
+	where
+		T: Serialize,
 	{
 		self.build(Method::Put, route, body, headers)
 	}
@@ -48,9 +45,10 @@ impl FakeReactor {
 		&self,
 		route: &str,
 		body: Option<T>,
-		headers: Option<Headers>
-	) -> Box<Future<Item=Response, Error=Response>>
-		where T: Serialize
+		headers: Option<Headers>,
+	) -> Result<Response, Response>
+	where
+		T: Serialize,
 	{
 		self.build(Method::Patch, route, body, headers)
 	}
@@ -59,9 +57,10 @@ impl FakeReactor {
 		&self,
 		route: &str,
 		body: Option<T>,
-		headers: Option<Headers>
-	) -> Box<Future<Item=Response, Error=Response>>
-		where T: Serialize
+		headers: Option<Headers>,
+	) -> Result<Response, Response>
+	where
+		T: Serialize,
 	{
 		self.build(Method::Delete, route, body, headers)
 	}
@@ -71,13 +70,14 @@ impl FakeReactor {
 		method: Method,
 		route: &str,
 		body: Option<T>,
-		mut headers: Option<Headers>
-	) -> Box<Future<Item=Response, Error=Response>>
-		where T: Serialize
+		mut headers: Option<Headers>,
+	) -> Result<Response, Response>
+	where
+		T: Serialize,
 	{
 		let body = match body {
 			Some(b) => Some(to_vec(&b).unwrap().into()),
-			None => Some(Body::default())
+			None => Some(Body::default()),
 		};
 
 		if headers.is_none() {
@@ -85,6 +85,7 @@ impl FakeReactor {
 		}
 
 		let headers = headers.unwrap();
+		let mut reactor = Core::new().expect("Could not start event loop");
 
 		let req = Request {
 			method: method.clone(),
@@ -94,21 +95,22 @@ impl FakeReactor {
 			body,
 			remote: None,
 			anyMap: AnyMap::new(),
-			handle: None,
+			handle: Some(reactor.handle()),
 		};
 
-
-		let matched = self.routes.matchRoute(req.path(), &method)
+		let matched = self
+			.routes
+			.matchRoute(req.path(), &method)
 			.expect(&format!("No Handler registered for Method::{}", method));
 
-		return ArcService::call(matched.handler, req, Response::new());
+		return reactor.run(ArcService::call(matched.handler, req, Response::new()));
 	}
 }
 
 #[cfg(test)]
 mod tests {
 	use impl_service::*;
-	use hyper::{StatusCode};
+	use hyper::StatusCode;
 	use futures::{Future, Stream};
 	use futures::prelude::async_block;
 	use proto::ArcService;
@@ -128,12 +130,12 @@ mod tests {
 	fn it_matches_the_correct_route_and_returns_the_correct_body() {
 		let routes = Router::new().get("/hello", AsyncService);
 
-		let fakereactor = FakeReactor {
-			routes
-		};
+		let fakereactor = FakeReactor { routes };
 
 		// assert it returns Ok
-		let result = fakereactor.get("/hello?lol=p", None).wait().expect("Should return ok");
+		let result = fakereactor
+			.get("/hello?lol=p", None)
+			.expect("Should return ok");
 		let body = result.body().concat2().wait().expect("Body should be Ok");
 		assert_eq!(&body[..], b"Hello World");
 	}
